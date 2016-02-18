e51eed44aafcf8f3639aecdb41418be9e91b4bf5
Michi F <prunebutt@gmail.com>
2016-02-18 10:52:26 +0100

    refined iteration algorithm. Still doesn't converge, though.

--- src/mdp.cpp
+++ src/mdp.cpp
@@ -2,7 +2,7 @@
 
 namespace mdp{
   void utilityIteration(std::vector<State>& stateList){
-	int foo = 20;
+	int foo = 200;
 
 	while(foo--){
 	  cout << "Iteration " << foo << ":" << endl;
@@ -10,21 +10,22 @@ namespace mdp{
 		std::vector<Action> actions =  stateList[i].getActions(); // Get the reference to the actions of the current state
 		cout << "\tState: " << stateList[i].getName() << endl;
 
+		double max_utility = 0;
 		for (int a=0; a<actions.size();a++){               // sum through utilities of actions
 		  std::vector<Edge> edges = actions[a].getEdges(); // get the reference to the edges of the current Action
-		  double utility, max_utility = 0;
+		  double utility = actions[a].getReward();
 		  for (int j=0; j<edges.size(); j++){
-			utility = edges[j].target_->utility * edges[j].probability_; // sum(p*U(target))
-			utility += actions[a].getReward()*edges[j].probability_;
-			utility += stateList[i].utility;
+			utility += edges[j].target_->utility * edges[j].probability_; // sum(p*U(target))
 			if (utility > max_utility)
 			  max_utility = utility;
 			//edges[j].target_->utility = utility; // assign the new utility value to the stateList
 		  } // end edge iteration
 		  cout << "\t\tAction: " << actions[a].getName() \
 			   << "\tutility: " << utility << endl;
-		  //stateList[i]->utility = utility; // assign the new utility value to the stateList
+		  if (utility > max_utility)
+			max_utility = utility;
 		} // end action iteration
+		stateList[i].utility = max_utility; // assign the new utility value to the stateList
 	  } // end state iteration
 	} // end while
   } // end utilityIteration
